8ef250bff552e24e2b2974eea5931ec934c8e02e
Alexandra Onțică <150956540+alexandraontica@users.noreply.github.com>
2024-05-15 14:08:37 +0300

    low_pass

--- numerical_music/low_pass.m
+++ numerical_music/low_pass.m
@@ -1,3 +1,14 @@
 function signal = low_pass(signal, fs, cutoff_freq)
-  signal = 0;
+  % signal = 0;
+  x_fft = fft(signal);
+
+  n = length(signal);
+  f = (0 : n - 1) .* (fs ./ n);
+  f = f';
+
+  mask = f < cutoff_freq;
+  x_filtered = x_fft .* mask;
+
+  signal = ifft(x_filtered);
+  signal = signal / max(abs(signal));
 endfunction
